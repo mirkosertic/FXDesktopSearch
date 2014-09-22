75c96961ab234e9aa98dd15c7cbb7f7d41c6beb0
Mirko Sertic <mirko.sertic@web.de>
2014-09-22 21:31:42 +0200

    Bug Fixing #1 Search suggestions produce no search result

--- src/main/java/de/mirkosertic/desktopsearch/QueryParser.java
+++ src/main/java/de/mirkosertic/desktopsearch/QueryParser.java
@@ -49,91 +49,41 @@ class QueryParser {
         return "";
     }
 
-    private boolean isValid(String aTerm) {
-        return !StringUtils.isEmpty(aTerm) && !"*".equals(aTerm) && !"?".equals(aTerm);
-    }
-
     public Query parse(String aQuery, String aSearchField) throws IOException {
 
-        List<String> theRequiredTerms = new ArrayList<>();
-        List<String> theNotRequiredTerms = new ArrayList<>();
-
-        boolean isNegated = false;
-        StringBuilder theCurrentTerm = new StringBuilder();
-
-        for (int i = 0; i < aQuery.length(); i++) {
-            char theCurrentChar = Character.toLowerCase(aQuery.charAt(i));
-            switch (theCurrentChar) {
-                case '-': {
-                    if (theCurrentTerm.length() == 0) {
-                        isNegated = true;
-                    } else {
-                        theCurrentTerm.append(theCurrentChar);
-                    }
-                    break;
-                }
-                case '+':
-                    if (theCurrentTerm.length() == 0) {
-                        isNegated = false;
-                    } else {
-                        theCurrentTerm.append(theCurrentChar);
-                    }
-                    break;
-                case ' ': {
-                    if (isValid(theCurrentTerm.toString())) {
-                        if (isNegated) {
-                            theNotRequiredTerms.add(theCurrentTerm.toString());
-                        } else {
-                            theRequiredTerms.add(theCurrentTerm.toString());
-                        }
-                    }
-                    theCurrentTerm = new StringBuilder();
-                    isNegated = false;
-                    break;
-                }
-                default: {
-                    theCurrentTerm.append(theCurrentChar);
-                    break;
-                }
-            }
-        }
-
-        if (isValid(theCurrentTerm.toString())) {
-            if (isNegated) {
-                theNotRequiredTerms.add(theCurrentTerm.toString());
-            } else {
-                theRequiredTerms.add(theCurrentTerm.toString());
-            }
-        }
+        QueryTokenizer theTokenizer = new QueryTokenizer(aQuery);
 
         // Now we have the terms, lets construct the query
 
         BooleanQuery theResult = new BooleanQuery();
 
-        if (!theRequiredTerms.isEmpty()) {
+        if (!theTokenizer.getNotRequiredTerms().isEmpty()) {
 
-            SpanQuery[] theSpans = new SpanQuery[theRequiredTerms.size()];
-            for (int i = 0; i < theRequiredTerms.size(); i++) {
-                String theTerm = theRequiredTerms.get(i);
+            List<SpanQuery> theSpans = new ArrayList<>();
+            for (String theTerm : theTokenizer.getRequiredTerms()) {
                 if (QueryUtils.isWildCard(theTerm)) {
-                    theSpans[i] = new SpanMultiTermQueryWrapper<>(new WildcardQuery(new Term(aSearchField, theTerm)));
+                    theSpans.add(new SpanMultiTermQueryWrapper<>(new WildcardQuery(new Term(aSearchField, theTerm))));
                 } else if (QueryUtils.isFuzzy(theTerm)) {
-                    theSpans[i] = new SpanMultiTermQueryWrapper<>(new FuzzyQuery(new Term(aSearchField, theTerm)));
+                    theSpans.add(new SpanMultiTermQueryWrapper<>(new FuzzyQuery(new Term(aSearchField, theTerm))));
                 } else {
-                    theSpans[i] = new SpanTermQuery(new Term(aSearchField, toToken(theTerm, aSearchField)));
+                    // Ok, we need to check of the token would be removed due to stopwords and so on
+                    String theTokenizedTerm = toToken(theTerm, aSearchField);
+                    if (!StringUtils.isEmpty(theTokenizedTerm)) {
+                        theSpans.add(new SpanTermQuery(new Term(aSearchField, theTokenizedTerm)));
+                    }
                 }
             }
 
             // This is the original span, so we boost it a lot
-            SpanQuery theExactMatchQuery = new SpanNearQuery(theSpans, 0, true);
+            SpanQuery theExactMatchQuery = new SpanNearQuery(theSpans.toArray(new SpanQuery[theSpans.size()]), 0, true);
             theExactMatchQuery.setBoost(61);
             theResult.add(theExactMatchQuery, BooleanClause.Occur.SHOULD);
 
-            // We expect a maximum edit distance of 20 between the searched terms in any order
+            // We expect a maximum edit distance of 10 between the searched terms in any order
             // This seems to be the most useful value
             int theMaxEditDistance = 10;
             for (int theSlop=0;theSlop<theMaxEditDistance;theSlop++) {
-                SpanQuery theNearQuery = new SpanNearQuery(theSpans, theSlop, false);
+                SpanQuery theNearQuery = new SpanNearQuery(theSpans.toArray(new SpanQuery[theSpans.size()]), theSlop, false);
                 theNearQuery.setBoost(50 + theMaxEditDistance - theSlop);
                 theResult.add(theNearQuery, BooleanClause.Occur.SHOULD);
             }
@@ -141,24 +91,31 @@ class QueryParser {
             // Finally, we just add simple term queries, but do not boost them
             // This makes sure that at least the searched terms
             // are found in the document
-            for (String theTerm : theRequiredTerms) {
+            for (String theTerm : theTokenizer.getRequiredTerms()) {
                 if (QueryUtils.isWildCard(theTerm)) {
                     theResult.add(new WildcardQuery(new Term(aSearchField, toToken(theTerm, aSearchField))),
                             BooleanClause.Occur.MUST);
                 } else if (QueryUtils.isFuzzy(theTerm)) {
                     theResult.add(new FuzzyQuery(new Term(aSearchField, theTerm)), BooleanClause.Occur.MUST);
                 } else {
-                    theResult
-                            .add(new TermQuery(new Term(aSearchField, toToken(theTerm, aSearchField))), BooleanClause.Occur.MUST);
+                    String theTokenizedTerm = toToken(theTerm, aSearchField);
+                    if (!StringUtils.isEmpty(theTokenizedTerm)) {
+                        theResult.add(new TermQuery(new Term(aSearchField, theTokenizedTerm)), BooleanClause.Occur.MUST);
+                    }
                 }
             }
         }
 
-        for (String theTerm : theNotRequiredTerms) {
+        for (String theTerm : theTokenizer.getNotRequiredTerms()) {
             if (QueryUtils.isWildCard(theTerm)) {
                 theResult.add(new WildcardQuery(new Term(aSearchField, theTerm)), BooleanClause.Occur.MUST_NOT);
+            } else if (QueryUtils.isFuzzy(theTerm)) {
+                theResult.add(new FuzzyQuery(new Term(aSearchField, theTerm)), BooleanClause.Occur.MUST_NOT);
             } else {
-                theResult.add(new TermQuery(new Term(aSearchField, toToken(theTerm, aSearchField))), BooleanClause.Occur.MUST_NOT);
+                String theTokenizedTerm = toToken(theTerm, aSearchField);
+                if (!StringUtils.isEmpty(theTokenizedTerm)) {
+                    theResult.add(new TermQuery(new Term(aSearchField, theTokenizedTerm)), BooleanClause.Occur.MUST_NOT);
+                }
             }
         }
 
---
+++ src/main/java/de/mirkosertic/desktopsearch/QueryTokenizer.java
@@ -0,0 +1,99 @@
+/**
+ * FreeDesktopSearch - A Search Engine for your Desktop
+ * Copyright (C) 2013 Mirko Sertic
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package de.mirkosertic.desktopsearch;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class QueryTokenizer {
+
+    private final List<String> requiredTerms;
+    private final List<String> notRequiredTerms;
+
+    QueryTokenizer(String aQuery) {
+
+        requiredTerms = new ArrayList<>();
+        notRequiredTerms = new ArrayList<>();
+
+        boolean isFirstChar = true;
+        boolean isNegated = false;
+        StringBuilder theCurrentTerm = new StringBuilder();
+
+        for (int i = 0; i < aQuery.length(); i++) {
+            char theCurrentChar = Character.toLowerCase(aQuery.charAt(i));
+            switch (theCurrentChar) {
+                case '-': {
+                    if (isFirstChar) {
+                        isFirstChar = false;
+                        isNegated = true;
+                    } else {
+                        theCurrentTerm.append(theCurrentChar);
+                    }
+                    break;
+                }
+                case '+':
+                    if (isFirstChar) {
+                        isNegated = false;
+                        isFirstChar = false;
+                    } else {
+                        theCurrentTerm.append(theCurrentChar);
+                    }
+                    break;
+                case ' ': {
+                    if (isValid(theCurrentTerm.toString())) {
+                        if (isNegated) {
+                            notRequiredTerms.add(theCurrentTerm.toString());
+                        } else {
+                            requiredTerms.add(theCurrentTerm.toString());
+                        }
+                    }
+                    theCurrentTerm = new StringBuilder();
+                    isNegated = false;
+                    isFirstChar = true;
+                    break;
+                }
+                default: {
+                    theCurrentTerm.append(theCurrentChar);
+                    isFirstChar = false;
+                    break;
+                }
+            }
+        }
+
+        if (isValid(theCurrentTerm.toString())) {
+            if (isNegated) {
+                notRequiredTerms.add(theCurrentTerm.toString());
+            } else {
+                requiredTerms.add(theCurrentTerm.toString());
+            }
+        }
+    }
+
+    static boolean isValid(String aTerm) {
+        return !StringUtils.isEmpty(aTerm) && !"*".equals(aTerm) && !"?".equals(aTerm);
+    }
+
+    public List<String> getRequiredTerms() {
+        return Collections.unmodifiableList(requiredTerms);
+    }
+
+    public List<String> getNotRequiredTerms() {
+        return Collections.unmodifiableList(notRequiredTerms);
+    }
+}
