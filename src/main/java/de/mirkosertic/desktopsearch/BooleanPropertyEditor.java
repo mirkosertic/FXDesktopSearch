6b540822015b281102683cb5461a6106bb23d7a2
Mirko Sertic <mirko.sertic@web.de>
2017-10-06 23:24:54 +0200

    #20 Optimize Preferences dialog

---
+++ src/main/java/de/mirkosertic/desktopsearch/BooleanPropertyEditor.java
@@ -0,0 +1,35 @@
+/**
+ * FreeDesktopSearch - A Search Engine for your Desktop
+ * Copyright (C) 2013 Mirko Sertic
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+package de.mirkosertic.desktopsearch;
+
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.CheckBox;
+import org.controlsfx.control.PropertySheet;
+import org.controlsfx.property.editor.AbstractPropertyEditor;
+
+public class BooleanPropertyEditor extends AbstractPropertyEditor<Boolean, CheckBox> {
+
+    public BooleanPropertyEditor(PropertySheet.Item property) {
+        super(property, new CheckBox());
+    }
+
+    @Override
+    protected ObservableValue<Boolean> getObservableValue() {
+        return getEditor().selectedProperty();
+    }
+
+    @Override
+    public void setValue(Boolean aBoolean) {
+        getEditor().selectedProperty().setValue(aBoolean);
+    }
+}
---
+++ src/main/java/de/mirkosertic/desktopsearch/PropertyEditorItem.java
@@ -0,0 +1,63 @@
+/**
+ * FreeDesktopSearch - A Search Engine for your Desktop
+ * Copyright (C) 2013 Mirko Sertic
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+package de.mirkosertic.desktopsearch;
+
+import javafx.beans.value.ObservableValue;
+import org.controlsfx.control.PropertySheet;
+import org.controlsfx.property.editor.PropertyEditor;
+
+import java.util.Optional;
+
+public abstract class PropertyEditorItem implements PropertySheet.Item {
+
+    private final Class type;
+    private final String category;
+    private final String name;
+    private final Class propertyEditor;
+
+    public PropertyEditorItem(Class aType, String aCategory, String aName, Class aPropertyEditor) {
+        type = aType;
+        category = aCategory;
+        name = aName;
+        propertyEditor = aPropertyEditor;
+    }
+
+    @Override
+    public Class<?> getType() {
+        return type;
+    }
+
+    @Override
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
+
+    @Override
+    public Optional<ObservableValue<? extends Object>> getObservableValue() {
+        return Optional.empty();
+    }
+
+    public Optional<Class<? extends PropertyEditor<?>>> getPropertyEditorClass() {
+        return Optional.of(propertyEditor);
+    }
+}
